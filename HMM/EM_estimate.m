6576f7db1471757b1e52286159f5be90a76b210c
tztz8888 <ecowen@126.com>
2015-12-01 15:58:14 -0500

    HMM README
    README file attached in HMM directory

--- HMM/EM_estimate.m
+++ HMM/EM_estimate.m
@@ -3,7 +3,6 @@ function [a,b,p] = EM_estimate(a, b, p, X, nIter)
 K= size(a,1);
 M= size(b,2);
 
-nIter=5;
 for i=1:nIter
   [Gamma, Xi] = E_step(a, b, p, X);
   [a, b, p] = M_step(Gamma, Xi, X, M, K);
