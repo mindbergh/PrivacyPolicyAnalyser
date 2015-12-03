a44950eac024eda2c6f5eee7d885c79e895e2769
mindbergh <mindbergh@gmail.com>
2015-12-03 14:26:11 -0500

    add template

--- www/ppa/views.py
+++ www/ppa/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
+def homepage(request):
+    context = {}
+
+    return render(request, 'ppa/index.html', context)
+
 # Create your views here.
--- www/www/urls.py
+++ www/www/urls.py
@@ -18,5 +18,5 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', 'ppa.views.homepage'),
 ]
