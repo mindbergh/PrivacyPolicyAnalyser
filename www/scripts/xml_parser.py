ab76ff444daee2f8a654f5ac82dcdb00f4c84ae0
mindbergh <mindbergh@gmail.com>
2015-12-04 17:11:07 -0500

    finish loading

--- www/scripts/xml_parser.py
+++ www/scripts/xml_parser.py
@@ -1,7 +1,6 @@
 import os
 import xml.etree.ElementTree as ET
 import sqlite3
-import json
 
 
 def parse_xml(filename):
@@ -25,7 +24,7 @@ def parse_xml(filename):
         l_secs.append(t_sec)
         t_sec = []
 
-    policy = (filename, l_secs)
+    policy = (os.path.basename(filename), l_secs)
 
     return policy
 
@@ -34,48 +33,10 @@ BASE_PATH = '..'
 DB_PATH = os.path.join(BASE_PATH, 'db.sqlite3')
 CORPUS_DIR = '../../corpus'
 INSERT_POLICY = "INSERT INTO policy (title) VALUES (?)"
-INSERT_SECTIONS = "INSERT INTO section (pid, substtle, subtext) VALUES (?, ?, ?)"
+INSERT_SECTIONS = "INSERT INTO section (pid, subtitle, subtext) VALUES (?, ?, ?)"
 
 
-def parse_xml(filename):
-    tree = ET.parse(filename)
-    root = tree.getroot()
-    policy = {}
-    conn = sqlite3.connect('../db.sqlite3')
-    c = conn.cursor()
-    c.execute("INSERT INTO policy (title) VALUES (?)", (filename,))
-    line = c.execute("SELECT * FROM policy WHERE title= '%s'" % filename)
-    pid = line.fetchone()[0]
-    for section in root.iter("SECTION"):
-        subtitle = section.find("SUBTITLE").text
-        subtext = section.find("SUBTEXT").text
-        policy[subtitle] = subtext
-        # print "subtitle: "+ subtitle
-        # print "subtext: "+ subtext
-        stmt = "INSERT INTO section (pid,subtitle,subtext) VALUES (%d,'%s','%s')" % (pid, subtitle, subtext)
-        print stmt
-        c.execute(stmt)
-        # print policy
-
-
-def policy_mapper(policy):
-    return json.dumps(policy[0])
-
-
-def section_mapper(policy):
-    p = dbc.execute("SELECT * FROM policy WHERE title = '%s'" % json.dumps(policy[0]))
-    pid = p.fetchone()[0]
-    return [(pid, json.dumps(subtitle), json.dumps(subtext)) for subtitle, subtext in policy[1]]
-
-
-def policy_loader(policies, dbc):
-    dbc.executemany(INSERT_POLICY,
-                    map(policy_mapper, policies))
-    dbc.executemany(INSERT_SECTIONS,
-                    map(section_mapper, policies))
-
-
-if __name__ == '__main__':
+def parse():
     policys = []
     files = [f for f in os.listdir(CORPUS_DIR)]
     for f in files:
@@ -83,9 +44,34 @@ if __name__ == '__main__':
             p = parse_xml(os.path.join(CORPUS_DIR, f))
             policys.append(p)
     print len(policys)
+    return policys
+
+
+def policy_mapper(policy):
+    return (policy[0], )
+
+
+def section_mapper(policy):
+    p = dbc.execute("SELECT * FROM policy WHERE title = '%s'" % policy[0])
+    pid = p.fetchone()[0]
+    return [(pid, subtitle, subtext) for subtitle, subtext in policy[1]]
+
+
+def policy_loader(policies, dbc):
+    dbc.executemany(INSERT_POLICY,
+                    map(policy_mapper, policies))
+
+    sections = [ section for policy in map(section_mapper, policies) for section in policy ]
+    dbc.executemany(INSERT_SECTIONS,
+                    sections)
+
+
+if __name__ == '__main__':
+    policies = parse()
 
     conn = sqlite3.connect(DB_PATH)
     dbc = conn.cursor()
 
-    policy_loader(policys, dbc)
+    policy_loader(policies, dbc)
     dbc.close()
+    conn.commit()
